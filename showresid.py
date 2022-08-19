75766d8177fb5f1838eef933d7fb687d1a7239a6
YanJordan <zeyin.yan@outlook.com>
2022-08-19 13:17:38 +0800

    Update showresid.py

--- showresid.py
+++ showresid.py
@@ -1,11 +1,14 @@
 from pymol import cmd
 
 
-def showresid(resnum,rad='4.0'):
+def showresid(resnum,chain='A',rad='4.0'):
+#def showresid(resnum,rad='4.0'):
     cmd.hide('everything','all')
-    cmd.select("centerres","resi "+resnum)
+    #cmd.select("centerres","resi "+resnum)
+    cmd.select("centerres","chain "+chain+" and (resi "+resnum+")")
     
-    cmd.select("ressite","(byres (resi "+ resnum + ")expand "+ rad +")")
+    cmd.select("ressite","(byres centerres expand "+ rad +")")
+    #cmd.select("ressite","(byres (chain " + chain + "and (resi "+ resnum + ")) expand "+ rad +")")
     cmd.show_as("wire","ressite")
     
     cmd.show_as("licorice","centerres")
