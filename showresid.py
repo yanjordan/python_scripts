f5835986cd75d8c9550cd9bee0d2d91c8c6ac2e3
YanJordan <zeyin.yan@ecp.fr>
2022-08-23 09:20:51 +0800

    Update showresid.py

--- showresid.py
+++ showresid.py
@@ -2,20 +2,18 @@ from pymol import cmd
 
 
 def showresid(resnum,chain='A',rad='4.0'):
-<<<<<<< HEAD
     cmd.hide('everything','all')
     cmd.select("centerres","resi "+resnum+" and (chain "+chain+")")
     
     cmd.select("ressite","(byres centerres expand "+ rad +")")
-=======
+
 #def showresid(resnum,rad='4.0'):
-    cmd.hide('everything','all')
-    #cmd.select("centerres","resi "+resnum)
-    cmd.select("centerres","chain "+chain+" and (resi "+resnum+")")
-    
-    cmd.select("ressite","(byres centerres expand "+ rad +")")
-    #cmd.select("ressite","(byres (chain " + chain + "and (resi "+ resnum + ")) expand "+ rad +")")
->>>>>>> 75766d8177fb5f1838eef933d7fb687d1a7239a6
+    #cmd.hide('everything','all')
+    ##cmd.select("centerres","resi "+resnum)
+    #cmd.select("centerres","chain "+chain+" and (resi "+resnum+")")
+    #
+    #cmd.select("ressite","(byres centerres expand "+ rad +")")
+    ##cmd.select("ressite","(byres (chain " + chain + "and (resi "+ resnum + ")) expand "+ rad +")")
     cmd.show_as("wire","ressite")
     
     cmd.show_as("licorice","centerres")
