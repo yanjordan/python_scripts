0659aae369199c3f7bd03b8fcc67bba731dd963c
YanJordan <zeyin.yan@ecp.fr>
2022-08-23 09:21:08 +0800

    Update showresid.py

--- showresid.py
+++ showresid.py
@@ -6,14 +6,6 @@ def showresid(resnum,chain='A',rad='4.0'):
     cmd.select("centerres","resi "+resnum+" and (chain "+chain+")")
     
     cmd.select("ressite","(byres centerres expand "+ rad +")")
-
-#def showresid(resnum,rad='4.0'):
-    #cmd.hide('everything','all')
-    ##cmd.select("centerres","resi "+resnum)
-    #cmd.select("centerres","chain "+chain+" and (resi "+resnum+")")
-    #
-    #cmd.select("ressite","(byres centerres expand "+ rad +")")
-    ##cmd.select("ressite","(byres (chain " + chain + "and (resi "+ resnum + ")) expand "+ rad +")")
     cmd.show_as("wire","ressite")
     
     cmd.show_as("licorice","centerres")
