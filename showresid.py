3cbd1d939573e7580e90ad6ab76cb4a3a21fe079
YanJordan <zeyin.yan@ecp.fr>
2022-08-23 09:44:31 +0800

    Update showresid.py

--- showresid.py
+++ showresid.py
@@ -1,8 +1,17 @@
 from pymol import cmd
 
-
-def showresid(resnum,chain='A',rad='4.0'):
+def str2cr(resstr): #input string to chain and residue num
+    if resstr.isdigit():
+        chain="A" # Chain A in default
+        resi=resstr
+    else:
+        chain=resstr[0:1]
+        resi=resstr[1:]
+    return chain, resi 
+    
+def showresid(resi, rad='4.0'):
     cmd.hide('everything','all')
+    chain, resnum=str2cr(resi)
     cmd.select("centerres","resi "+resnum+" and (chain "+chain+")")
     
     cmd.select("ressite","(byres centerres expand "+ rad +")")
