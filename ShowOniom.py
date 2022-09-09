3b0a3cb98a6c8d9b95dff4f87a2b3ec0dcf01516
YanJordan <zeyin.yan@outlook.com>
2022-09-09 17:01:11 +0800

    Create ShowOniom.py

---
+++ ShowOniom.py
@@ -0,0 +1,62 @@
+from pymol import cmd
+
+def str2cr(resstr): #input string to chain and residue num
+    if resstr.isdigit():
+        chain="A" # Chain A in default
+        resi=resstr
+    else:
+        chain=resstr[0:1]
+        resi=resstr[1:]
+    return chain, resi 
+    
+def ShowOniom(resi, mrad='3.0',rrad='0.0',high='0.0'):
+    #cmd.hide('everything','all')
+    cmd.bg_color('1 1 1')
+    cmd.set('orthoscopic','1')
+    cmd.set('fog','1')
+    
+    #cmd.load(name+'.pdb')
+    
+    cmd.set('cartoon_oval_quality', '100')
+    cmd.set('sphere_quality', '2')
+    cmd.set('stick_quality', '30')
+    cmd.set('surface_quality', '3')
+    cmd.set('sphere_scale', '0.24')
+    #cmd.set('sphere_mode', '4')
+
+    cmd.hide('lines')
+    cmd.hide('nonbonded')
+    cmd.hide('spheres')
+    cmd.hide('licorice')
+    
+    chain, resnum=str2cr(resi)
+    
+    cmd.select("centerres","resi "+resnum)
+    
+    cmd.select("highlayer","(byres centerres expand "+ high +")")
+    cmd.select("mediumlayer","(byres centerres expand "+ mrad +" and (not highlayer))")
+    cmd.select("relaxed","(byres centerres expand "+ rrad +")")
+    
+    #cmd.copy_to("HL","highlayer")
+    #cmd.show('line','all')
+    #high 
+    
+    cmd.set('stick_radius', '0.2')
+    cmd.show('sticks','relaxed')
+    #mediwum layer
+    cmd.show("sticks","mediumlayer")
+    cmd.set('line_width','1')
+    cmd.util.cba('154',"mediumlayer")
+    #highlayer
+    cmd.set('stick_radius', '0.15',"highlayer")
+    cmd.set('sphere_scale', '0.25',"highlayer")
+    
+    cmd.show("licorice","highlayer")
+    cmd.show("spheres","highlayer")
+    
+    cmd.util.cba('6',"relaxed")
+    
+    cmd.zoom("mediumlayer",animate=-1)
+    
+cmd.extend('ShowOniom', ShowOniom)
+#ShowOniom('5p9i_a_flip_ligH', '701')
