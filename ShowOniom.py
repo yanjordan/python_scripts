7c2d336aa6159f242c7c9cb0d9977fd7c8c51644
YanJordan <zeyin.yan@ecp.fr>
2022-09-20 19:00:43 +0800

    Update ShowOniom.py

--- ShowOniom.py
+++ ShowOniom.py
@@ -8,7 +8,32 @@ def str2cr(resstr): #input string to chain and residue num
         chain=resstr[0:1]
         resi=resstr[1:]
     return chain, resi 
-    
+
+def reslist(selection,output='N'):
+	listres=""
+	counter=0
+	sel=selection
+	objs=cmd.get_object_list(sel)
+	
+	for a in range(len(objs)):
+		m1=cmd.get_model(sel+" and "+objs[a])
+		for x in range(len(m1.atom)):
+			if m1.atom[x-1].resi!=m1.atom[x].resi:
+				listres+="%s %s%s " %(m1.atom[x].chain, m1.atom[x].resn, m1.atom[x].resi)
+				counter+=1
+				
+	print("Residues in '%s': %s" %(selection, counter))
+	
+	if output=="S": 
+		print(listres)
+	if output=="P":
+		print(listres)
+		f=open('Listres_'+selection+'.txt','w')
+		f.write("Residues in '%s': %s\n" % (selection, counter))
+		f.write(listres)
+		f.close()
+		print("Results saved in Listres__%s.txt" %(selection))
+
 def ShowOniom(resi, mrad='3.0',rrad='0.0',high='0.0'):
     #cmd.hide('everything','all')
     cmd.bg_color('1 1 1')
@@ -36,10 +61,9 @@ def ShowOniom(resi, mrad='3.0',rrad='0.0',high='0.0'):
     cmd.select("highlayer","(byres centerres expand "+ high +")")
     cmd.select("mediumlayer","(byres centerres expand "+ mrad +" and (not highlayer))")
     cmd.select("relaxed","(byres centerres expand "+ rrad +")")
-    
-    #cmd.copy_to("HL","highlayer")
-    #cmd.show('line','all')
-    #high 
+	
+    reslist('highlayer','P')
+    reslist('mediumlayer','P')
     
     cmd.set('stick_radius', '0.2')
     cmd.show('sticks','relaxed')
